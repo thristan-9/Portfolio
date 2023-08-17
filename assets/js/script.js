0288f0fdf6a662436aff970a9a53e7cd10103daa
thristan gabriel pereira deretti <thristanxyz@gmail.com>
2023-08-17 16:11:10 -0300

    save

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-/*=============== WINDOW ===============*/
+/*=============== About expand ===============*/
 
 const aboutBody = document.querySelector(".about__body");
 const aboutExpandWrapper = document.querySelector(".about_expand-wrapper");
@@ -12,3 +12,28 @@ function aboutExpandToggle() {
         aboutBody.classList.add('accordion-open')
     }
 }
+
+
+/*=============== EMail ===============*/
+
+const contactForm = document.getElementById("contact-form"),
+        contactMessage = document.getElementById("contact-message");
+    
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("service_3sssnvd", "template_qfderfk", "#contact-form", "hcGC8xD7_6muwcAQY")
+    .then(() => {
+        contactMessage.textContent = "Message sent successfully ✅";
+
+        setTimeout(() => {
+            contactMessage.textContent = "";
+        }, 4000)
+
+        contactForm.reset()
+    }, () => {
+        contactMessage.textContent = "Message not sent (service error) ❌";
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail);
